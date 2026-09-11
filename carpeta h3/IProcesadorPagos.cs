d335b8ae39e4c09069fcfd6965f779dd9345636a
Abigail Venecia Geronimo Jimenez <85464736+Abigail1014@users.noreply.github.com>
2026-09-11 10:29:37 -0400

    Delete carpeta h3 directory

--- carpeta h3/IProcesadorPagos.cs
+++
@@ -1,9 +0,0 @@
-// H3 · CON ADAPTER.
-// Este es el contrato que el resto del sistema de Créditos espera para procesar un pago.
-
-namespace Creditos.ConAdapter;
-
-public interface IProcesadorPagos
-{
-    bool ProcesarPago(Credito credito, int numeroCuota);
-}
